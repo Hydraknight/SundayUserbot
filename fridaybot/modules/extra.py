9a70131c87938d7552b16ce4b38ed9c700d6894d
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-17 11:29:34 +0530

    Update extra.py

--- fridaybot/modules/extra.py
+++ fridaybot/modules/extra.py
@@ -1,6 +1,5 @@
 import asyncio
-import time
-from collections import deque
+import time 
 
 from telethon.tl.functions.channels import LeaveChannelRequest
 
@@ -11,23 +10,12 @@ from fridaybot.utils import friday_on_cmd
 @friday.on(friday_on_cmd("leave$"))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`I iz Leaving dis Lol Group kek!`")
+        await e.edit("`My Boss is Leaving This Basterd Chat!`")
         time.sleep(3)
         if "-" in str(e.chat_id):
             await bot(LeaveChannelRequest(e.chat_id))
         else:
-            await e.edit("`But Boss! This is Not A Chat`")
+            await e.edit("`But, Boss! This is Not A Chat`")
 
 
 
-@friday.on(friday_on_cmd("yo$"))
-# @register(outgoing=True, pattern="^yo$")
-async def Yooo(e):
-    t = "yo"
-    for j in range(15):
-        t = t[:-1] + "oo"
-        await e.edit(t)
-
-
-
-CMD_HELP.update({"leave": "Leave a Chat"})
