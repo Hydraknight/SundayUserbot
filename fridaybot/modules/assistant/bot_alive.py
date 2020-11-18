4c0d35680c619c6d9d07634d91250cde9a9d45f8
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-18 09:30:29 +0530

    Update bot_alive.py

--- fridaybot/modules/assistant/bot_alive.py
+++ fridaybot/modules/assistant/bot_alive.py
@@ -1,15 +1,4 @@
-#    Copyright (C) Midhun KM 2020
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 
 from fridaybot import ALIVE_NAME
@@ -18,7 +7,7 @@ from fridaybot.modules import currentversion
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/a86263ea08437c5b633e7.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
-pm_caption += f"➥ **My Boss** : [{DEFAULTUSER}](tg://user?id=1445368987) \n"
+pm_caption += f"➥ **My Boss** : [{DEFAULTUSER}](tg://user?id=1412254339) \n"
 
 # only Owner Can Use it
 @assistant_cmd("alive", is_args=False)
