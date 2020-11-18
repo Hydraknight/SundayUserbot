f72eec6262935542dcb4eea0604d41abcc63f843
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-18 11:39:55 +0530

    Update __init__.py

--- fridaybot/modules/__init__.py
+++ fridaybot/modules/__init__.py
@@ -9,10 +9,6 @@ issudousing = Config.SUDO_USERS
 islogokay = Config.PRIVATE_GROUP_ID
 isdbfine = Var.DB_URI
 isherokuokay = Var.HEROKU_APP_NAME
-gdriveisshit = Config.AUTH_TOKEN_DATA
-wttrapi = Config.OPEN_WEATHER_MAP_APPID
-rmbg = Config.REM_BG_API_KEY
-hmmok = Config.LYDIA_API
 currentversion = "3.0"
 if issudousing:
     amiusingsudo = "Active ✅"
@@ -29,37 +25,16 @@ if isherokuokay:
 else:
     riplife = "Not Connected ❌"
 
-if gdriveisshit:
-    wearenoob = "Active ✅"
-else:
-    wearenoob = "Inactive ❌"
-
-if rmbg:
-    gendu = "Added ✅"
-else:
-    gendu = "Not Added ❌"
-
-if wttrapi:
-    starknoobs = "Added ✅"
-else:
-    starknoobs = "Not Added ❌"
-
-if hmmok:
-    meiko = "Added ✅"
-else:
-    meiko = "Not Added ❌"
-
 if isdbfine:
     dbstats = "Fine ✅"
 else:
     dbstats = "Not Fine ❌"
 
 inlinestats = (
-    f"✘ SHOWING GUJJU STATS ✘\n"
+    f"✘ SHOWING USERBUTT STATS ✘\n"
     f"VERSION = {currentversion} \n"
     f"DATABASE = {dbstats} \n"
     f"SUDO = {amiusingsudo} \n"
     f"LOG-CHAT = {logchat} \n"
-    f"HEROKU = {riplife} \n"
-    f"G-DRIVE = {wearenoob}"
+    f"HEROKU = {riplife} "
 )
