f14ecea6aa8533aa0038ddea6b58fb6ceb1a28c1
Starkgangs <Starkgangz@gmail.com>
2020-11-19 12:50:52 +0000

    pylint: auto fixes

--- fridaybot/modules/nsfw-detector.py
+++ fridaybot/modules/nsfw-detector.py
@@ -1,11 +1,12 @@
 import requests
-import json
+
 from fridaybot.utils import friday_on_cmd, sudo_cmd
+
 
 @friday.on(friday_on_cmd(pattern=r"nsfw"))
 @friday.on(sudo_cmd(pattern=r"nsfw", allow_sudo=True))
 async def nsfw(event):
-    url = 'https://nsfw-categorize.it/api/upload'
+    url = "https://nsfw-categorize.it/api/upload"
     replymsg = await event.get_reply_message()
     photo = None
     if replymsg and replymsg.media:
@@ -14,11 +15,13 @@ async def nsfw(event):
         elif "image" in replymsg.media.document.mime_type.split("/"):
             photo = await borg.download_file(replymsg.media.document)
         else:
-            await event.edit("`Reply To Image`")   
+            await event.edit("`Reply To Image`")
     if photo:
-        files = {'image':(f'{photo}', open(f'{photo}', 'rb'))}
+        files = {"image": (f"{photo}", open(f"{photo}", "rb"))}
         r = requests.post(url, files=files).json()
-        if r["status"]=='OK':
-            await event.edit("This image is classified as "+str(r["data"]["classification"]))
+        if r["status"] == "OK":
+            await event.edit(
+                "This image is classified as " + str(r["data"]["classification"])
+            )
         else:
             await event.edit("Response UnsucessFull. Try Again.")
